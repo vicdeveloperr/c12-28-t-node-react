c40eb2b1d095a4b75fa075124d7c4162aea929b5
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-07-11 20:43:56 -0300

    Limpieza de algunos archivos

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './Views/Home'
+import Home from "./Views/Home";
 
 function App() {
-  const [count, _setCount] = useState(0)
-
   return (
     <>
-    <Home/>
-      <h1>{count}</h1>
+      <Home />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
