cdaf49edea8c61595b38ecce790317d0d308f2c5
fcicerelli <fcicerellitic@gmail.com>
2023-07-11 08:20:05 -0300

    Corrección en archivo .env

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,8 +5,7 @@ const Sequelize = require('sequelize');
 // dotenv.config();
 require('dotenv').config()
 
-//const db = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
-const db = new Sequelize(`products`, `root`, ``, {
+const db = new Sequelize(`${process.env.DB_NAME}`, `${process.env.DB_USER}`, `${process.env.DB_PASSWORD}`, {
   host: 'localhost',
   dialect: 'mysql',
   define: {
