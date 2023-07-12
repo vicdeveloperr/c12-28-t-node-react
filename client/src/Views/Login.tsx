c0154b26f78b97039a040d9d22b2d50f68f24d49
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-07-11 21:44:02 -0600

    Updating login design

--- client/src/Views/Login.tsx
+++ client/src/Views/Login.tsx
@@ -6,38 +6,30 @@ const Login = () => {
 
         {/*<RegisterCompleted />*/}
 
- <div className="relative">
-            <a className="bg-gray-600" href="">Inicio </a>
-            <a href="">/ Login</a>
 
-            <div className="border-black rounded-sm border-[2px] flex justify-center items-center ">
-                <div className="w-[546px] h-[360px] bg-slate-600">
-                    <img src="" alt="" />
+        <div className="rounded-sm flex flex-col justify-center items-center h-screen relative ">
+                <div className="w-[546px] h-[360px] ">
+                    <img className="rounded-t-[15px] shadow-lg" src="https://images.unsplash.com/photo-1546432507-bd9e20b3c78b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="" />
                 </div>
-                <div>
-                    <div className="bg-[#EEEBEB] pl-[64px] pr-[68px]">
-                        <h2 className="text-center">Inicio de sesión</h2>
 
-                        <div className="flex flex-col justify-center item-center">
-                            <p className="pl-3">Usuario</p>
-                            <input type="text" className="rounded-lg border-gray border-[1px]"/>
-                            <h2 className="pl-3">Contraseña</h2>
-                            <input type="text" className="rounded-lg border-gray border-[1px]"/>
-                            <button className="bg-[#3A3A3A] border-[gray] border-[1px] rounded-full w-[149px] p-1 text-2xl text-white">Aceptar</button>
-                        </div>
-                            
-
-                        <p>No tienes cuenta?</p>
-                        <button className="bg-[#3A3A3A] border-[gray] border-[1px] rounded-full w-[149px] p-1 text-lg text-white">Registrate aquí</button>
-                        <p>Olvidaste tu contraseña?</p>
+                <div className="w-[546px]">
+                    <div className="bg-[#EEEBEB] p-14 flex flex-col items-center justify-center rounded-b-[15px] shadow-lg">
+                        <h2 className="text-center font-bold text-xl">Inicio de sesión</h2>
+                            <div className="flex flex-col justify-center item-center mt-6">
+                                <p className="text-center">Usuario</p>
+                                <input type="text" className="rounded-lg border-gray border-[1px]"/>
+                                <h2 className="text-center mt-2">Contraseña</h2>
+                                <input type="text" className="rounded-lg border-gray border-[1px]"/>
+                                <button className="mt-2 bg-[#1B3847] border-[gray] border-[1px] rounded-full p-1 text-lg text-white ml-2 mr-2">Aceptar</button>
+                            </div>
+                        <p className="mt-4">No tienes cuenta?</p>
+                        <button className=" mt-2 bg-[#1B3847] border-[gray] border-[1px] rounded-full w-[149px] p-1 text-lg text-white">Registrate aquí</button>
+                        <p className="mt-2">Olvidaste tu contraseña?</p>
                     </div>
                 </div>
             </div>
-        </div>
-
-        <div className="bg-[#D9D9D9] absolute bottom-0 w-full flex justify-center">
+            <div className="bg-[#D9D9D9] absolute bottom-0 w-full flex justify-center">
                 <p>Lorem ipsum is placeholder  text commonly</p>
-                <img src="" alt="" className="w-[211px] h-[57px]"/>
             </div>
     </>
     )
