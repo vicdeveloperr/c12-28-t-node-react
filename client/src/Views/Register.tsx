a83ee37ef77a9242d21c0afdd772f1972cca5cca
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-07-13 10:31:44 -0300

    Fix: modificar algunos estilos de los componentes

--- client/src/Views/Register.tsx
+++ client/src/Views/Register.tsx
@@ -1,39 +1,67 @@
-const Register = () => {
-    return (
-        <>
-        <div className="relative">
-            <a className="bg-gray-600" href="">Inicio </a>
-            <a href="">/ Crear cuenta</a>
+import Footer from "../components/common/Footer";
+import NavBar from "../components/common/NavBar";
 
-            <div className="border-black rounded-sm border-[2px] flex justify-center items-center ">
-                <div className="w-[546px] h-[360px] bg-slate-600">
-                    <img src="" alt="" />
-                </div>
-                <div>
-                    <div className="bg-[#EEEBEB]">
-                        <h2>Crear cuenta</h2>
-                        <p>Nombre Completo</p>
-                        <input type="text" className="rounded-lg border-gray border-[1px]"/>
-                        <h2>Correo electrónico</h2>
-                        <input type="text" className="rounded-lg border-gray border-[1px]"/>
-                        <h2>Contraseña</h2>
-                        <input type="text" placeholder="Debe tener al menos 6 caracteres" className="rounded-lg border-gray border-[1px]" />
-                        <h2>Vuelve a escribir la contraseña</h2>
-                        <input type="text" className="rounded-lg border-gray border-[1px]"/>
-                        <p>Al crear una cuenta, aceptas las Condiciones de Uso y
-                        el Aviso de Privacidad de Lorem Ipsum.</p>
-                        <button className="bg-[#3A3A3A] border-[gray] border-[1px] rounded-full w-[149px] p-1 text-2xl text-white">Aceptar</button>
-                    </div>
-                </div>
+export default function Register() {
+  return (
+    <>
+      <NavBar />
+      <div className="py-8 w-[346px] m-auto">
+        <div className="rounded-sm flex flex-col justify-center items-center ">
+          <img
+            className="rounded-t-[15px] shadow-lg"
+            src="https://images.unsplash.com/photo-1546432507-bd9e20b3c78b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            alt=""
+          />
+
+          <div className="bg-white-color px-14 py-8 flex flex-col gap-6 items-center justify-center rounded-b-[15px] shadow-sm">
+            <h2 className="text-center font-bold text-xl">Crear Cuenta</h2>
+            <div className="flex flex-col justify-center item-center gap-2">
+              <label>
+                <h3>Nombre Completo</h3>
+                <input
+                  type="text"
+                  required
+                  className="py-1 px-2 rounded-lg border-gray border-[1px] focus:outline-none"
+                />
+              </label>
+              <label>
+                <h3>Correo electrónico</h3>
+                <input
+                  type="text"
+                  required
+                  className="py-1 px-2 rounded-lg border-gray border-[1px] focus:outline-none"
+                />
+              </label>
+              <label>
+                <h3>Contraseña</h3>
+                <input
+                  type="text"
+                  placeholder="Debe tener al menos 6 caracteres"
+                  required
+                  className="py-1 px-2 rounded-lg border-gray border-[1px] focus:outline-none"
+                />
+              </label>
+              <label>
+                <h3>Vuelve a escribir la contraseña</h3>
+                <input
+                  type="text"
+                  required
+                  className="py-1 px-2 rounded-lg border-gray border-[1px] focus:outline-none"
+                />
+              </label>
+              <small className="text-gray-500">
+                Al crear una cuenta, aceptas las Condiciones de Uso y el Aviso
+                de Privacidad de Lorem Ipsum.
+              </small>
+              <button className="bg-tertiary-color rounded-full p-2 text-lg text-white">
+                Aceptar
+              </button>
             </div>
+          </div>
         </div>
+      </div>
 
-        <div className="bg-[#D9D9D9] absolute bottom-0 w-full flex justify-center">
-                <p>Lorem ipsum is placeholder  text commonly</p>
-                <img src="" alt="" className="w-[211px] h-[57px]"/>
-            </div>
-        </>
-    )
+      <Footer />
+    </>
+  );
 }
-
-export default Register
