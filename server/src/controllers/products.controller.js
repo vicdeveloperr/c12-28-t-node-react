f24b2c378e959c473b5fcb57daf9b333dc03094d
fcicerelli <fcicerellitic@gmail.com>
2023-07-19 20:27:36 -0300

    producst.conroller methods editProductPhoto and deleteProduct

--- server/src/controllers/products.controller.js
+++ server/src/controllers/products.controller.js
@@ -86,13 +86,71 @@ const createProduct = async (req, res) => {
   }
 };
 
+const editProductPhoto = async (req, res) => {
+  const { idProduct, name, description, stock, price, topic_1, detail_1, topic_2, detail_2, topic_3, detail_3,
+    topic_4, detail_4, topic_5, detail_5, topic_6, detail_6, topic_7, detail_7, topic_8, detail_8,
+    idCategoryProduct, idUserProduct, photos } = req.body;
+  try {
+    const product = await Product.findByPk(idProduct, {
+      include: { model: Photo, as: 'photos' }
+    });
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    product.name = name;
+    product.description = description;
+    product.stock = stock;
+    product.price = price;
+    product.topic_1 = topic_1;
+    product.detail_1 = detail_1;
+    product.topic_2 = topic_2;
+    product.detail_2 = detail_2;
+    product.topic_3 = topic_3;
+    product.detail_3 = detail_3;
+    product.topic_4 = topic_4;
+    product.detail_4 = detail_4;
+    product.topic_5 = topic_5;
+    product.detail_5 = detail_5;
+    product.topic_6 = topic_6;
+    product.detail_6 = detail_6;
+    product.topic_7 = topic_7;
+    product.detail_7 = detail_7;
+    product.topic_8 = topic_8;
+    product.detail_8 = detail_8;
+    product.idCategoryProduct = idCategoryProduct;
+    product.idUserProduct = idUserProduct;
+    await product.save();
+
+    const photosToUpdate = product.photos.map((photo, i) => {
+      photo.name = photos[i].name;
+      return photo;
+    });
+
+    await Promise.all(photosToUpdate.map((photo) => photo.save()));
+
+    res.status(200).json({ message: 'Foto editada exitosamente' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error al editar la foto del producto' });
+  }
+};
+
 const deleteProduct = async (req, res) => {
   const { id } = req.params;
 
   try {
     const product = await Product.findOne({ where: { idProduct: id } });
     if (product) {
-      await Product.destroy({ where: { idProduct: id } })
+      /* const photosToDelete = await Photo.findAll({ where: { idPhoto: product_photo.photoIdPhoto product.idProduct } });
+      for (const photo of photosToDelete) {
+        await photo.destroy();
+      } */
+      await Product.destroy({
+        where: { idProduct: id },
+        include: Photo
+      })
       return res.status(202).send('Product deleted');
     } else {
       return res.status(400).send('product not found');
@@ -103,7 +161,7 @@ const deleteProduct = async (req, res) => {
 }
 
 const getCategories = async (req, res) => {
-  console.log("zzz             zzzz              zzz             zzz")
+  console.log("\nzzz             zzzz              zzz             zzz\n")
   try {
     const categories = await Category.findAll();
     res.status(200).json(categories);
@@ -116,6 +174,7 @@ module.exports = {
   getProducts,
   getProduct,
   createProduct,
+  editProductPhoto,
   deleteProduct,
   getCategories
 }
--- server/src/routes/products.routes.js
+++ server/src/routes/products.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 // const controllers = require('../controllers/index');
-const { createProduct, getProducts, getProduct, deleteProduct, getCategories } = require('../controllers/products.controller');
+const { createProduct, getProducts, getProduct, editProductPhoto, deleteProduct, getCategories } = require('../controllers/products.controller');
 const { verifyToken, isUser } = require('../middlewares/authJwt');
 
 const router = Router();
@@ -10,6 +10,7 @@ router.get('/', getProducts);
 router.get('/:id', getProduct);
 
 router.post('/', /* [verifyToken, isUser], */ createProduct);
+router.put('/:id', editProductPhoto);
 router.delete('/:id', /* [verifyToken, isUser], */ deleteProduct);
 // router.put('/productId', controllers.postProduct);
 router.get('/categories', getCategories);
