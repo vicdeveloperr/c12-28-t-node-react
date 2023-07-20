14c029d8c7985b0c984d6c4d60881e5480b6f0d5
Kevin <108099077+PixelGenetics@users.noreply.github.com>
2023-07-20 17:01:39 -0600

    Saving itemss

--- client/src/components/common/TopBar.tsx
+++ client/src/components/common/TopBar.tsx
@@ -47,7 +47,7 @@ function TopBar() {
     const searchBarElement: HTMLElement | null = document.querySelector("#searchBar");
     if(searchBarElement) {
       searchBarElement.classList.remove("hidden")
-      //searchBarElement.classList.add("flex")
+      searchBarElement.classList.add("flex")
     }
     hiddenElements(["logoElement", "sideBarNavToglerElement", "searchBarToglerElement"])
   }
