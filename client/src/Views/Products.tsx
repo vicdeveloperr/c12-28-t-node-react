2df521ceed84e9bfa55b4e9922c6c5c84c52b310
AlondraCena <alondra.cena@gmail.com>
2023-07-25 01:40:55 -0300

    landing responsive (ver)

--- client/src/Views/Products.tsx
+++ client/src/Views/Products.tsx
@@ -11,22 +11,21 @@ export default function Products() {
                     <h1 className="text-h3 text-secondary-color font-extrabold mb-4">Productos</h1>
                 </div>
                 <div className="grid justify-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                <div className="flex">
-            <ProductCard />
-          </div>
-          <div className="flex">
-            <ProductCard />
-          </div>
-          <div className="flex">
-            <ProductCard />
-          </div>
-          <div className="flex">
-            <ProductCard />
-          </div>
-          <div className="flex">
-            <ProductCard />
-          </div>
-
+                  <div className="flex">
+                    <ProductCard />
+                  </div>
+                  <div className="flex">
+                    <ProductCard />
+                  </div>
+                  <div className="flex">
+                    <ProductCard />
+                  </div>
+                  <div className="flex">
+                    <ProductCard />
+                  </div>
+                  <div className="flex">
+                    <ProductCard />
+                  </div>
                 </div>
             </div>
          <Footer />
