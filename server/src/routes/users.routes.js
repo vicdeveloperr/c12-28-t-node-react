ca76c92cde08866f2c895cda1e499c2a7f006285
Ernest2104 <villagra9@gmail.com>
2023-07-27 20:12:56 -0300

    conflictos resueltos

--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -6,13 +6,8 @@ const { verifyToken, isAdmin } = require('../middlewares/authJwt');
 const router = Router();
 
 router.get('/', getUsers);
-<<<<<<< HEAD
 router.get('/mail', getUserByEmail);
 router.get('/:id', getUser);
 router.put('/:id', updateUser);
-=======
-router.get('/:email', getUserByEmail);
-router.post('/', [verifyToken, isAdmin], createUser)
->>>>>>> 9cae88e9c30a281eac7cecb5d41f1db50e835041
 
 module.exports = router;
